896f0db12b924156fb9b6f73f1e81bbd40763430
Henrik Skov Midtiby <hemi@mmmi.sdu.dk>
2018-02-21 14:15:15 +0100

    Initial commit.

---
+++ vision_guided_landing.py
@@ -0,0 +1,33 @@
+# Import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import time
+import cv2
+
+# Initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+
+# Allot the camera to warmup
+time.sleep(0.1)
+
+# Capture frames from the camera
+for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # gra the raw NumPy array representing the image, then initialize
+    # the timestamp and occupied/unoccupied text.
+    image = frame.array
+
+    # show the fram
+    cv2.imshow("Frame", image)
+    key = cv2.waitKey(1) & 0xFF
+
+    # clear the stream in preparation for the nect frame
+    rawCapture.truncate(0)
+
+    # if the 'q' key was pressed, break from the loop
+    if key == ord('q'):
+        break
+
+
